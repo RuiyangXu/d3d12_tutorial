e00fe36e0c55cb428af753c33d4b892ea5298de9
Ryan <ruiyang.xu12@gmail.com>
2025-06-21 20:27:40 -0700

    window framework setup

---
+++ src/DXLearnSample.cpp
@@ -0,0 +1,21 @@
+#include "DXLearnSample.h"
+
+DXLearnSample::DXLearnSample(UINT width, UINT height, std::wstring name) : DXSample(width, height, name)
+{
+}
+
+void DXLearnSample::OnInit()
+{
+}
+
+void DXLearnSample::OnUpdate()
+{
+}
+
+void DXLearnSample::OnRender()
+{
+}
+
+void DXLearnSample::OnDestroy()
+{
+}
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+﻿#include "Win32Application.h"
+#include <windows.h>
+#include <d3d12.h>
+#include "DXLearnSample.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+    DXLearnSample sample{ 800, 600, L"D3D12" };
+    return Win32Application::Run(reinterpret_cast<DXSample*>(&sample), hInstance, nCmdShow);
+}
